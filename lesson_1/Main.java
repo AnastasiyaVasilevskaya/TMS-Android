603ed9412ac6066585ea7e8a5c79993d48fb71fb
Anastasiya Vasilevskaya <94404108+nastyavasilevskaya@users.noreply.github.com>
2023-12-13 22:38:59 +0300

    Add files via upload

---
+++ lesson_1/Main.java
@@ -0,0 +1,25 @@
+package lesson_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // 1 задание
+        System.out.println("Введите два числа: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("Сумма чисел: " + (a + b));
+
+        // 2 задание
+        System.out.println("Hello World");
+        System.out.println();
+
+        // 3 задание
+        System.out.println("Введите Ваше имя: ");
+        String user = scan.next();
+        System.out.println("Hello " + user);
+
+
+    }
+}
